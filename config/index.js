71d3ca4b47d57169af738b0dca55289067c8a9cd
huangyitinging <49449937+huangyitinging@users.noreply.github.com>
2019-10-31 23:18:37 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -43,7 +43,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/animalworld_vue/dist/',
+    assetsPublicPath: '/animalworld_vuex/dist/',
 
     /**
      * Source Maps
